c890cdde368196309e0f77abb2c03b721741f9e7
Bpaul311 <bizimapaul777.com>
2023-12-19 12:46:54 -0800

    adding tasks 5

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,7 +6,7 @@ class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new square.
+        """Initialize a new Square.
         Args:
             size (int): The size of the new square.
         """
@@ -16,6 +16,18 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        """returns the area of a square"""
+        return self.__size ** 2
