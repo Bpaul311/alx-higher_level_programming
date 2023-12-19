54ddefe16a6166f46f8fb9febb8f847b90f59146
Bpaul311 <bizimapaul777.com>
2023-12-19 11:41:38 -0800

    adding file 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a module for square"""
+
+
+class Square:
+    """Represents a Square"""
+     def __init__(self, size):
+        """constructor object.
+        Args:
+        size: length of the side of the square
+        """
+        self.__size = size
