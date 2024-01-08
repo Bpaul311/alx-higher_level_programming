9c4f37db841891287a0f824f1981b9db4143fded
Bpaul311 <bizimapaul777.com>
2024-01-08 15:19:51 -0800

    removing spaces

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines an inherited list class called MyList"""
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """inherits list class"""
+    """Inherits list class"""
 
     def print_sorted(self):
-        """prints a sorted list"""
+        """Prints a list in sorted ascending order."""
         print(sorted(self))
